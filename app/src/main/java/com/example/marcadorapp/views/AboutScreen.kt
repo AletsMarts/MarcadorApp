81ce3d55663dedd8247529e054a4b0608918bdb4
Alfredo Reza <alfredo_rezas@hotmail.com>
2024-12-18 00:44:11 -0600

    Ya esta la pantalla cabrones about

--- app/src/main/java/com/example/marcadorapp/views/AboutScreen.kt
+++ app/src/main/java/com/example/marcadorapp/views/AboutScreen.kt
@@ -1,14 +1,16 @@
 package com.example.marcadorapp.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.marcadorapp.R // Asegúrate de que la imagen está en res/drawable y de que el nombre del recurso es correcto.
 
 @Composable
 fun AboutScreen(navController: NavController) {
@@ -19,11 +21,27 @@ fun AboutScreen(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Acerca de la Aplicación", style = androidx.compose.material3.MaterialTheme.typography.headlineMedium)
+        Text(
+            text = "Acerca de Volley Score",
+            style = androidx.compose.material3.MaterialTheme.typography.headlineMedium
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Esta aplicación es un marcador para gestionar resultados y registrar partidos de manera sencilla y eficiente.")
+        Text(text = "Materia: Desarrollo de Aplicaciones en Android")
+        Text(text = "Docente: José Luis Mota Espeleta")
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Desarrollado por: Tu Nombre")
+        Text(text = "Aplicación hecha por:")
+        Text(text = "Alejandro Martínez")
+        Text(text = "Ronaldo Puentes")
+        Text(text = "Neo Maldonado")
+        Text(text = "Alfredo Reza")
+        Spacer(modifier = Modifier.height(32.dp))
+        Image(
+            painter = painterResource(id = R.drawable.unnamed), // Reemplaza 'your_image' por el nombre de tu recurso
+            contentDescription = "Imagen Acerca de",
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp)
+        )
         Spacer(modifier = Modifier.height(32.dp))
         Button(onClick = { navController.navigate("home") }) {
             Text(text = "Volver al Inicio")
